46162e1d81ce6796ab60fe74ee701061db0c9c68
Eduardo Vitorino <eduardovitorino.contato@gmail.com>
2026-02-07 08:41:53 -0300

    Primeiro commit: Iniciando projeto

---
+++ src/main/java/com/evenhub/EvenHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.evenhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evenhub/EvenHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
